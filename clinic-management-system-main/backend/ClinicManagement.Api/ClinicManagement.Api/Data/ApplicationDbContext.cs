40d1bd5cebbb95d2458b78563c24e76754319678
Tâm Thanh <lebinhnhi113@gmail.com>
2026-02-19 02:59:33 +0700

    [Patient] Create Patient model

---
+++ clinic-management-system-main/backend/ClinicManagement.Api/ClinicManagement.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ClinicManagement.Api.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace ClinicManagement.Api.Data
+{
+    public class ApplicationDbContext
+    {
+        public DbSet<Patient> Patients { get; set; }    
+    }
+}
