6a4f6b28092cec9327ddab5ce1fb725b4d285685
ThanhNhan611 <tnhann6117@gmail.com>
2026-03-22 21:48:06 +0700

    Create Department.cs

---
+++ backend/ClinicManagement.Api/ClinicManagement.Api/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClinicManagement.Api.Models;
+
+public class Department
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public ICollection<Doctor> Doctors { get; set; } = new List<Doctor>();
+}
