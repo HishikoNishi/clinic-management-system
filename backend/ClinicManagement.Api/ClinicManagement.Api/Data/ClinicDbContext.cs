671594041f299adf196a646f9b33b73ba01411d6
ThanhNhan611 <tnhann6117@gmail.com>
2026-03-23 21:41:00 +0700

    Update ClinicDbContext.cs

--- backend/ClinicManagement.Api/ClinicManagement.Api/Data/ClinicDbContext.cs
+++ backend/ClinicManagement.Api/ClinicManagement.Api/Data/ClinicDbContext.cs
@@ -220,6 +220,19 @@ namespace ClinicManagement.Api.Data
                       .OnDelete(DeleteBehavior.Cascade);
             });
 
+            modelBuilder.Entity<Department>().HasData(
+     new Department { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Nội" },
+     new Department { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Ngoại" },
+     new Department { Id = Guid.Parse("33333333-3333-3333-3333-333333333333"), Name = "Nhi" },
+     new Department { Id = Guid.Parse("44444444-4444-4444-4444-444444444444"), Name = "Sản" },
+     new Department { Id = Guid.Parse("55555555-5555-5555-5555-555555555555"), Name = "Mắt" },
+     new Department { Id = Guid.Parse("66666666-6666-6666-6666-666666666666"), Name = "Tai Mũi Họng" },
+     new Department { Id = Guid.Parse("77777777-7777-7777-7777-777777777777"), Name = "Da Liễu" },
+     new Department { Id = Guid.Parse("88888888-8888-8888-8888-888888888888"), Name = "Tim Mạch" },
+     new Department { Id = Guid.Parse("99999999-9999-9999-9999-999999999999"), Name = "Thần Kinh" },
+     new Department { Id = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"), Name = "Răng Hàm Mặt" }
+ );
+
 
 
 
