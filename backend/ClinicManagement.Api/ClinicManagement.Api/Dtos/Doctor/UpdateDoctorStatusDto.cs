e0549ea9821f6e4cec8e47f22dbfbb696fdf78d5
ThanhNhan611 <tnhann6117@gmail.com>
2026-03-27 21:35:20 +0700

    Create UpdateDoctorStatusDto.cs

---
+++ backend/ClinicManagement.Api/ClinicManagement.Api/Dtos/Doctor/UpdateDoctorStatusDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ClinicManagement.Api.Models;
+
+namespace ClinicManagement.Api.Dtos.Doctor
+{
+    public class UpdateDoctorStatusDto
+    {
+        public DoctorStatus Status { get; set; }
+    }
+
+}
+
