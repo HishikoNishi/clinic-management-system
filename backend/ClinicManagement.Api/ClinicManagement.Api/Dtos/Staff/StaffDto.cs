6824c7b4650c180bf93dc3d4925fa4f00a552034
ThanhNhan611 <tnhann6117@gmail.com>
2026-04-01 23:52:30 +0700

    Update StaffDto.cs

--- backend/ClinicManagement.Api/ClinicManagement.Api/Dtos/Staff/StaffDto.cs
+++ backend/ClinicManagement.Api/ClinicManagement.Api/Dtos/Staff/StaffDto.cs
@@ -4,7 +4,9 @@ public class StaffDto
     public string Code { get; set; } = null!;
     public string FullName { get; set; } = null!;
     public string Role { get; set; } = null!;
+    public string? Position { get; set; }
     public bool IsActive { get; set; }
     public string Username { get; set; } = null!;
+    public string? AvatarUrl { get; set; }
     public DateTime CreatedAt { get; set; }
 }
